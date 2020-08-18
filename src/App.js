49ef9c89bc83f236a8b058c125f134d3b6bae56c
Kike <kike@Kikes-MacBook-Pro.local>
2020-08-18 10:52:24 +0200

    Fix data link

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import styled, { createGlobalStyle } from 'styled-components'
 import {apolloColors, apolloFonts} from './data/apollo30styles'
 //-Images
 import logoImg from './assets/images/apollo30Logo.png'
+//-Components
+import SideNav from './components/SideNav'
 
 
 //STYLES
 //-Vars
 const leftSideWidth = "360px"
-const topFoldStripHeight = "150px"
 
 //-Components
 const GlobalStyle = createGlobalStyle`
@@ -26,68 +27,6 @@ const GlobalStyle = createGlobalStyle`
   button{
     border: none;
     outline: none;
-  }
-`
-
-const SideNav = styled.div`
-  position: fixed;
-  left: 0;
-  top: 0;
-  bottom: 0;
-  width: ${leftSideWidth};
-  height: 100vh;
-
-  .sidenavTop{
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    width: 100%;
-    height: calc(100% - ${topFoldStripHeight});
-    background: ${apolloColors.light};
-
-    .fullMenuContainer{
-      position: absolute;
-      top: 50%;
-      left: 120px;
-      transform: translateY(-50%);
-
-      .menuHeading{
-        ${apolloFonts.textSpecial};
-      }
-      .menuOptions{
-        margin-top: 20px;
-        list-style: none;
-        ${apolloFonts.subheading02};
-
-        li{
-          margin: 10px 0;
-
-          a{
-            text-decoration: none;
-            color: ${apolloColors.dark};
-          }
-        }
-      }
-    }
-  }
-  .sidenavBottom{
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    width: 100%;
-    height: ${topFoldStripHeight};
-    background: ${apolloColors.dark};
-
-    .sidenavBottomText{
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      color: ${apolloColors.light};
-      ${apolloFonts.textSmall}
-    }
   }
 `
 
@@ -181,31 +120,7 @@ function App() {
     <>
       <GlobalStyle/>
 
-      <SideNav>
-        <div className="sidenavTop">
-          <div className="fullMenuContainer">
-            <div className="menuHeading">Menu</div>
-            <ul className="menuOptions">
-              <li className="active">
-                <a href="#launches">Launches</a>
-              </li>
-              <li>
-                <a href="#services">Services</a>
-              </li>
-              <li>
-                <a href="#tripulation">Tripulation</a>
-              </li>
-              <li>
-                <a href="#base">Base</a>
-              </li>
-            </ul>
-          </div>
-
-        </div>
-        <div className="sidenavBottom">
-          <div className="sidenavBottomText">Scroll Down</div>
-        </div>
-      </SideNav>
+      <SideNav/>
 
       <ContentContainer>
 
@@ -235,7 +150,7 @@ function App() {
         </TopFold>
 
         <LaunchesSection>
-          
+
 
         </LaunchesSection>
 
--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -3,7 +3,7 @@
 import React from 'react'
 import styled from 'styled-components'
 //-Styles
-import {apolloColors, apolloFonts} from './data/apollo30styles'
+import {apolloColors, apolloFonts} from '../data/apollo30styles'
 
 
 //STYLES
