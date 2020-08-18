ec194cdae8fe7a8384105a7bf13db5f18fccaa56
Kike <kike@Kikes-MacBook-Pro.local>
2020-08-18 11:52:20 +0200

    Structure And Style Lauches

--- src/App.js
+++ src/App.js
@@ -105,11 +105,66 @@ const TopFold = styled.section`
     }  
   }
 `
-const LaunchesSection = styled.section`
+const FirstSectionSpace = styled.div`
   width: 100%;
-  padding: 130px;
+  height: 60px;
+  background: ${apolloColors.light};
+`
+const Section = styled.section`
+  width: 100%;
+  padding: 70px 130px 130px;
   background: ${apolloColors.light};
 
+  .sectionTitle{
+    color: ${apolloColors.dark};
+    ${apolloFonts.heading02};
+  }
+  .sectionDescription{
+    margin-top: 30px;
+    color: ${apolloColors.dark};
+    ${apolloFonts.textLarge};
+  }
+
+  .listOfClients{
+    margin-top: 50px;
+    width: 500px;
+
+    .listTitle{
+      padding-left: 10px;
+      ${apolloFonts.subheading03};
+    }
+    .divisionLine{
+      width: 100%;
+      height: 2px;
+      background: ${apolloColors.dark};
+    }
+    .topLine{
+      margin-top: 20px;
+    }
+
+    .tab{
+      position: relative;
+      border-bottom: 2px solid ${apolloColors.dark};
+      width: 100%;
+      padding: 20px 10px;
+      cursor: pointer;
+
+      .tabTitle{
+        display: inline-block;
+        color: ${apolloColors.dark};
+        ${apolloFonts.subheading01};
+      }
+      .tabRole{
+        position: absolute;
+        top: 50%;
+        right: 10px;
+        transform: translateY(-50%);
+        ${apolloFonts.textSmall};
+        opacity: 0.33;
+      }
+    }
+
+  }
 
 `
 
@@ -149,10 +204,88 @@ function App() {
 
         </TopFold>
 
-        <LaunchesSection>
+        <FirstSectionSpace/>
 
+        <Section>
+          <h2 className="sectionTitle">Launches</h2>
+          <p className="sectionDescription">Here are some non-confidential launches we have picked from our archives</p>
 
-        </LaunchesSection>
+          <div className="listOfClients list2020">
+            <h5 className="listTitle">2020</h5>
+
+            <div className="divisionLine topLine"></div>
+            <div className="tab">
+                <h3 className="tabTitle">Trick.Wtf</h3>
+                <div className="tabRole">Branding & Development</div>
+            </div>
+            <div className="tab">
+                <h3 className="tabTitle">Three Marks Coffee</h3>
+                <div className="tabRole">E-commerce Development</div>
+            </div>
+            <div className="tab">
+                <h3 className="tabTitle">Nike Shox</h3>
+                <div className="tabRole">Branded Content Campaign</div>
+            </div>
+            <div className="tab">
+                <h3 className="tabTitle">Mucho Muchacho</h3>
+                <div className="tabRole">Vinyl Cover & Website</div>
+            </div>
+            <div className="tab">
+                <h3 className="tabTitle">Sita Abellán</h3>
+                <div className="tabRole">Branding</div>
+            </div>
+            <div className="tab">
+                <h3 className="tabTitle">New Icons</h3>
+                <div className="tabRole">Branding</div>
+            </div>
+            <div className="tab">
+                <h3 className="tabTitle">La Proa 04</h3>
+                <div className="tabRole">Branding & Website</div>
+            </div>
+            <div className="tab">
+                <h3 className="tabTitle">Chip Baker Films</h3>
+                <div className="tabRole">Branding & Website</div>
+            </div>
+          </div>
+
+          <div className="listOfClients list2019">
+            <h5 className="listTitle">2019</h5>
+
+            <div className="divisionLine topLine"></div>
+            <div className="tab">
+                <h3 className="tabTitle">Uniko. "Model Agency"</h3>
+                <div className="tabRole">Rebranding</div>
+            </div>
+            <div className="tab">
+                <h3 className="tabTitle">Scranny's House</h3>
+                <div className="tabRole">Branding</div>
+            </div>
+            <div className="tab">
+                <h3 className="tabTitle">The New Storytellers</h3>
+                <div className="tabRole">Website</div>
+            </div>
+            <div className="tab">
+                <h3 className="tabTitle">Premis Butaca</h3>
+                <div className="tabRole">Campaing, Web, Video & Photography</div>
+            </div>
+            <div className="tab">
+                <h3 className="tabTitle">Owl Institute</h3>
+                <div className="tabRole">Branding & Website</div>
+            </div>
+            <div className="tab">
+                <h3 className="tabTitle">Cafes Bou</h3>
+                <div className="tabRole">Packaging Design</div>
+            </div>
+          </div>
+
+        </Section>
+
+        <Section>
+          
+          <h2 className="sectionTitle">Services</h2>
+          <p className="sectionDescription">We believe that in our current era, there’s a saturation of stimuli. This requires differentiation through tailored content.</p>
+
+        </Section>
 
       </ContentContainer>
     </>
